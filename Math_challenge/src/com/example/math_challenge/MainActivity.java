889b96c9d0a5fc508d1dbc1b7e4af4c52e8b8ae5
jesus- <chusma.ellagarto@gmail.com>
2014-04-09 15:55:24 +0800

    fix problems with build path

--- Math_challenge/src/com/example/math_challenge/MainActivity.java
+++ Math_challenge/src/com/example/math_challenge/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.math_challenge;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v4.app.Fragment;
+//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.Activity;
 import android.content.Intent;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 
 	
@@ -62,19 +63,19 @@ public class MainActivity extends ActionBarActivity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
+//	public static class PlaceholderFragment extends Fragment {
+//
+//		public PlaceholderFragment() {
+//		}
+//
+//		@Override
+//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//				Bundle savedInstanceState) {
+//			View rootView = inflater.inflate(R.layout.fragment_main, container,
+//					false);
+//			return rootView;
+//		}
+//	}
 
 
 }
